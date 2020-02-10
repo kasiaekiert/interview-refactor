0ec740980df826564e1f3b414763113980803d74
Kasia <kasia.ekiert@gmail.com>
2020-02-10 10:27:34 +0100

    EpisodeSerializer - boolean instead of null

--- app/serializer/episode_serializer.rb
+++ app/serializer/episode_serializer.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
+
 class EpisodeSerializer < ActiveModel::Serializer
   attributes :id, :title, :episode, :watched
 
+  def watched
+    object.watched.present?
+  end
+
   # def test
-  #   "test #{object.id}" 
+  #   "test #{object.id}"
   # end
 end
